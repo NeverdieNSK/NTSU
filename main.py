a56f5df30ce96867c05ed0e34125fc08cc6bceb6
Anton <darkneverdie@mail.ru>
2020-10-27 13:13:06 +0700

    зайти в вебинар

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import secret
+from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Chrome(executable_path='chromedriver.exe')
 driver. get('https://dispace.edu.nstu.ru')
@@ -19,3 +20,13 @@ driver.implicitly_wait(10)
 button = driver.find_element_by_name('callback_2')
 button.click()
 driver.implicitly_wait(10)
+# Мы в диспейсе, щёлкнуть на учебный процесс
+btn = driver.find_element_by_css_selector ('a[data-target="mobile-menu"]')
+btn.click()
+time.sleep(3)
+btn = driver.find_element_by_css_selector ('ul.sidenav>li:nth-child(6)>a')
+btn.click()
+#Зайти в Вебинар
+webinar = driver.find_element_by_css_selector('a[event_id="34723"]')
+mo = ActionChains(driver).move_to_element(webinar)
+mo.perform()
